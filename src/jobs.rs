e6b89448920dcbaaa7245c93fc012d4d0bbd84d9
Mike Shaver <mike.shaver@gmail.com>
2019-04-14 13:44:05 -0400

    Fill in Job structures from CSV

--- src/jobs.rs
+++ src/jobs.rs
@@ -1,3 +1,13 @@
+/*
+    TODO:
+    - test from string CSV
+    - validate SkillRatings
+    - print all
+    - JSON
+    - move to lib?
+    - load talents
+*/
+
 use std::collections::HashMap;
 use std::fs::File;
 use std::path::Path;
@@ -5,50 +15,53 @@ use std::path::Path;
 #[derive(Debug, Default)]
 pub struct SkillRating {
     pub name: String,          // Name of the skill
-    pub ratings: Vec<u32>      // Ratings at levels 1 through 32
+    pub ratings: [u32; 32]      // Ratings at levels 1 through 32
 }
 
 #[derive(Debug, Default)]
 pub struct Job {
     pub name: String,
-    pub skill_ratings: [SkillRating; 3]
+    pub skill_ratings: Vec<SkillRating>
 }
 
 pub type JobMap = HashMap<String, Job>;
 
 impl Job {
+    fn fill_rating(&mut self, rank: u32, name: String, rating: u32) {
+        let ratings = &mut self.skill_ratings;
+        let rank = (rank - 1) as usize;
+
+        if name.eq("Null") { return; }
+
+        match ratings.iter_mut().find(|sr| sr.name.eq(&name)) {
+            Some(sr) => {
+                sr.ratings[rank] = rating;
+                println!("setting {}:{}[{}] to {}", &self.name, &sr.name, rank, rating);
+            }
+            None => {
+                assert_ne!(ratings.len(), 3);
+                let mut sr = SkillRating { name: name, ratings: [0; 32]};
+                sr.ratings[rank] = rating;
+                ratings.push(sr);
+            }
+        }
+    }
 }
 
 #[derive(Debug, Deserialize)]
 struct JobSkillRecord {
-    #[serde(rename = "Rank")]
-    rank: u32,
-
-    #[serde(rename = "1-Name")]
-    name1: String,
-
-    #[serde(rename = "1-Num")]
-    rating1: u32,
-
-    #[serde(rename = "2-Name")]
-    name2: String,
-
-    #[serde(rename = "2-Num")]
-    rating2: u32,
-
-    #[serde(rename = "3-Name")]
-    name3: String,
-
-    #[serde(rename = "3-Num")]
-    rating3: u32,
-
-    #[serde(rename = "Job")]
-    job_name: String
+    #[serde(rename = "Rank")]   rank: u32,
+    #[serde(rename = "1-Name")] name1: String,
+    #[serde(rename = "1-Num")]  rating1: u32,
+    #[serde(rename = "2-Name")] name2: String,
+    #[serde(rename = "2-Num")]  rating2: u32,
+    #[serde(rename = "3-Name")] name3: String,
+    #[serde(rename = "3-Num")]  rating3: u32,
+    #[serde(rename = "Job")]    job_name: String
 }
 
 fn load_job_skills<T: std::io::Read>(rdr: T, map: &mut JobMap) -> Result<usize, csv::Error> {
 
-    const SINGLE_JOB : &str = "Commander";
     let mut rdr = csv::ReaderBuilder::new()
         .delimiter(b':')
         .from_reader(rdr);
@@ -56,10 +69,13 @@ fn load_job_skills<T: std::io::Read>(rdr: T, map: &mut JobMap) -> Result<usize, 
     for result in rdr.deserialize() {
         let record: JobSkillRecord = result?;
         let name = &record.job_name;
-        if name == SINGLE_JOB {
-            let mut _job = map.entry(name.clone()).or_insert(Job { name: name.clone(), ..Default::default()});
-            println!("{:?}", record);
-        }
+        let job = map.entry(name.clone())
+            .or_insert(Job { name: name.clone(), ..Default::default()});
+
+        job.fill_rating(record.rank, record.name1, record.rating1);
+        job.fill_rating(record.rank, record.name2, record.rating2);
+        if record.name3.eq("Null") { continue; }
+        job.fill_rating(record.rank, record.name3, record.rating3);
     }
 
     Result::Ok(map.len())
@@ -71,13 +87,6 @@ pub fn load_jobs(skill_ratings_file: &str) -> Result<JobMap, std::io::Error> {
 
     let mut jobs = JobMap::new();
     load_job_skills(File::open(&path)?, &mut jobs)?;
-
-/*
-    for result in rdr.records() {
-        let record = result?;
-        println!("record: {:?}", record);
-    }
-*/
 
     Result::Ok(jobs)
 }
