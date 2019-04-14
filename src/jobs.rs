6591e80ba7d6113fa7790106c232b939202ab0a9
Mike Shaver <mike.shaver@gmail.com>
2019-04-14 14:02:13 -0400

    Add basic tests.

--- src/jobs.rs
+++ src/jobs.rs
@@ -90,3 +90,46 @@ pub fn load_jobs(skill_ratings_file: &str) -> Result<JobMap, std::io::Error> {
 
     Result::Ok(jobs)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_SKILLS_CSV: &str = 
+"Rank:1-Name:1-Num:2-Name:2-Num:3-Name:3-Num:Job
+1:Blades:2:Evasion:1:Stealth:1:Assassin
+2:Blades:3:Evasion:1:Stealth:3:Assassin
+3:Blades:4:Evasion:1:Stealth:3:Assassin
+4:Blades:4:Evasion:1:Stealth:4:Assassin
+5:Blades:5:Evasion:1:Stealth:4:Assassin
+6:Blades:5:Evasion:1:Stealth:5:Assassin
+7:Blades:5:Evasion:1:Stealth:6:Assassin
+8:Blades:6:Evasion:1:Stealth:6:Assassin
+9:Blades:6:Evasion:1:Stealth:7:Assassin
+1:Pistols:2:Doctor:2:Null:0:Combat Medic
+2:Pistols:3:Doctor:4:Null:0:Combat Medic
+3:Pistols:3:Doctor:5:Null:0:Combat Medic
+4:Pistols:4:Doctor:5:Null:0:Combat Medic
+5:Pistols:4:Doctor:6:Null:0:Combat Medic
+6:Pistols:4:Doctor:7:Null:0:Combat Medic
+7:Pistols:4:Doctor:8:Null:0:Combat Medic";
+
+    #[test]
+    fn test_load_skills() {
+        let csv = TEST_SKILLS_CSV.as_bytes();
+        let mut j = JobMap::new();
+
+        load_job_skills(csv, &mut j).unwrap();
+
+        let aj = &j["Assassin"];
+        let cmj = &j["Combat Medic"];
+        assert_eq!(aj.name, ("Assassin"));
+        assert_eq!(aj.skill_ratings.len(), 3);
+
+        let aj_blades = aj.skill_ratings.iter().find(|sr| sr.name.eq("Blades")).unwrap();
+        assert_eq!(aj_blades.ratings[2], 4);
+        assert_eq!(aj_blades.ratings[8], 6);
+
+        assert_eq!(cmj.skill_ratings.len(), 2);
+    }
+}
